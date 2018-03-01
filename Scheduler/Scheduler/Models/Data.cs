2c28ef854bec9e6e7cf837693c9a0957332a4742
Stas Rivkin <slavariv@gmail.com>
2018-03-01 20:17:04 +0200

    adding objects and inputs

---
+++ Scheduler/Scheduler/Models/Data.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scheduler.Models
+{
+    class Data
+    {
+        public Data(Vehicle[] vehicles, Ride[] rides)
+        {
+            Vehicles = vehicles;
+            Rides = rides;
+        }
+
+        public Vehicle[] Vehicles { get; }
+        public Ride[] Rides { get; }
+    }
+}
---
+++ Scheduler/Scheduler/Parser.cs
@@ -0,0 +1,23 @@
+﻿using Scheduler.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace Scheduler
+{
+    public static class Parser
+    {
+        public static Data Read(string input)
+        {
+            //File.ReadLines(input)
+            //    .Take(1)
+            //    .Select(i =>
+            //    {
+            //        var vehicles = new Vehicle[];
+            //        var data = new Data();
+            //    });
+        }
+    }
+}
